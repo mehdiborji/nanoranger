4471315b5e9a97a3d1b22bb1f4b1f3a6005fd1a4
Livius Penter <62758571+shaka87@users.noreply.github.com>
2023-02-11 15:57:50 -0500

    Create pipeline_O2_CAR.py

---
+++ pipeline_O2_CAR.py
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH -c 16
+#SBATCH --mem=32G
+#SBATCH -t 1:00:00
+#SBATCH -p short
+#SBATCH -o poreranger_job_%A.out
+
+module load gcc/9.2.0
+module load python/3.8.12
+
+echo 'inputfq =' $1
+echo 'outdir =' $2
+echo 'sample =' $3
+echo 'cores =' $4
+
+python3.8 ./pipeline.py --s \
+--i $1 --o $2 --e $3 --c $4 \
+--t data/CAR.fa \
+--g data/CAR.fa
