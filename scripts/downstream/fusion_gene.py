e30d15447a71527f584575c4108d73686ec0997b
mehdiborji <borjijiji@gmail.com>
2024-05-29 23:45:21 -0400

    format

--- scripts/downstream/fusion_gene.py
+++ scripts/downstream/fusion_gene.py
@@ -2,24 +2,41 @@
 
 import pandas as pd
 import pysam
-import sys
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--bam', type=str, required=True, help='tagged bam file for processing')
-parser.add_argument('--output', type=str, required=True, help='output file')
+parser.add_argument(
+    "--bam", type=str, required=True, help="tagged bam file for processing"
+)
+parser.add_argument("--output", type=str, required=True, help="output file")
 
 args = parser.parse_args()
 
-samfile = pysam.AlignmentFile(args.bam, 'r')
+samfile = pysam.AlignmentFile(args.bam, "r")
 alignments = []
 for read in samfile.fetch():
-    alignments.append([read.reference_start, read.reference_end, read.reference_name,
-                   read.qlen, read.rlen, read.get_tag('CB'), read.get_tag('UB')])
-    
+    alignments.append(
+        [
+            read.reference_start,
+            read.reference_end,
+            read.reference_name,
+            read.qlen,
+            read.rlen,
+            read.get_tag("CB"),
+            read.get_tag("UB"),
+        ]
+    )
+
 seqs = pd.DataFrame(alignments)
 
-seqs.columns = ['ref_start', 'ref_end', 'ref_name', 'query_length', 'read_length', 'BC', 'UMI']
+seqs.columns = [
+    "ref_start",
+    "ref_end",
+    "ref_name",
+    "query_length",
+    "read_length",
+    "BC",
+    "UMI",
+]
 
-seqs.to_csv(args.output,index=None)
-
+seqs.to_csv(args.output, index=None)
